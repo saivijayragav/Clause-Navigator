dafb9f081d1a6058aec67275fc48e819b9d46696
unknown <saivijayragav@gmail.com>
2025-08-10 22:54:58 +0530

    Editing the prompt

--- main.py
+++ main.py
@@ -31,33 +31,6 @@ class QueryRequest(BaseModel):
 class QueryResponse(BaseModel):
     answers: List[str]
 
-# Gemini generation function
-def generate_answer_gemini(question: str, chunks: list):
-    context = "\n\n".join(chunks[:3])  # Top 3 chunks
-    prompt = f"""
-    You are an assistant to help users navigate documents and answer their queries.
-
-    Use only the following context to answer the question.
-    You can infer answers from the documents on your own.
-    Context:
-    {context}
-
-    Question: {question}
-
-    Instructions:
-    - Based ONLY on the above context, answer the question with clear and concise sentences.
-    - Keep the sentences concise.
-    - Each sentence should answer one distinct sub-question or point.
-    - Do NOT use any JSON, bullet points, numbers, or other list formatting.
-    - Do NOT use special character or escape characters
-    - Do NOT add any introductory or concluding text—only the answer sentences.
-
-    Answer now:
-    """
-    response = model.generate_content(prompt)
-    return response.text.strip()
-
-
 def makeGeminiCall(request):
     global model, KEY1, KEY2 
     try:
@@ -110,21 +83,8 @@ def parse_to_list(input_data):
 
 @app.post("/api/v1/hackrx/run")
 async def run_submission(request: QueryRequest, token: str = Depends(JWTBearer())):
-    print(request)
-    # Extract
-    # rag = RAG()
-    # rag.create_faiss_index(request.documents)  # Index document
-    # print("Creating faiss complete: ", time.time() - start)
-    # answers = []
-    # for question in request.questions:
-    #     chunks = rag.retrieval(question)  # RAG-style context retrieval
-    #     answer = generate_answer_gemini(question, chunks)
-    #     answers.append(answer)
-    start = time.time()
     answers = makeGeminiCall(request)
-    print(answers)
-    answers = parse_to_list(answers)
-    print("Generate gemini answers complete: ", time.time() - start) 
+    answers = parse_to_list(input("Answer: "))
     return QueryResponse(answers=answers)
 
 def log_to_json(request: QueryRequest, filename="query_log.json"):
@@ -149,7 +109,6 @@ def log_to_json(request: QueryRequest, filename="query_log.json"):
     with open(filename, mode="w", encoding="utf-8") as file:
         json.dump(data, file, indent=2)
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
-
+    uvicorn.run("main:app", host="0.0.0.0", port=8000)
 
 
